fb1a8678192953417edffcedf068a3d4a3657f3d
Joe Esposito <joe@joeyespo.com>
2015-07-08 23:16:42 -0400

    Add acorns.

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var showClickToStart;
 
 var player = {
   playing: false,
+  playTime: 0,
   score: 0,
   x: game.width / 2,
   y: game.height - 30,
@@ -28,18 +29,32 @@ var player = {
 var mouseX = null;
 var keysDown = {left: false, right: false};
 var bullets = [];
-var enemies = [];
+var acorns = [];
 var particles = [];
 
 function newGame() {
   player.playing = true;
-  // TODO: newEnemyGenerator()?
+  player.playTime = 0;
+  // TODO: newAcornGenerator()?
 }
 
 function endGame() {
   player.playing = false;
   player.played = true;
   clickLock = 30;
+}
+
+function newAcorn() {
+  var r = 16;
+  acorns.push({
+    r: r,
+    x: helpers.randInt(r, game.width - r),
+    y: -r,
+    vx: helpers.randInt(1, 5) * (helpers.randInt(0, 1) ? -1 : 1),
+    vy: 0,
+    angle: 0,
+    rotation: 0.1
+  });
 }
 
 // TODO: Mobile controls
@@ -109,6 +124,9 @@ game.update(function (t) {
     clickLock -= 1;
   }
 
+  // Update total play-time
+  player.playTime += 1;
+
   // Update bullets
   for (var b = 0; b < bullets.length; b++) {
     bullets[b].y -= bullets[b].s;
@@ -174,6 +192,32 @@ game.update(function (t) {
       p--;
     }
   }
+
+  // Update acorns
+  for (var a = 0; a < acorns.length; a++) {
+    acorns[a].x += acorns[a].vx;
+    if (acorns[a].x + acorns[a].r > game.width && acorns[a].vx > 0 ||
+        acorns[a].x - acorns[a].r < 0 && acorns[a].vx < 0) {
+      acorns[a].vx = -acorns[a].vx;
+    }
+    acorns[a].y += acorns[a].vy;
+    acorns[a].vy += GRAVITY;
+    if (acorns[a].y > player.y - acorns[a].r && acorns[a].vy > 0) {
+      acorns[a].vy = Math.min(-acorns[a].vy * 0.8, -6);
+    }
+    acorns[a].angle += acorns[a].rotation;
+    if (acorns[a].angle < -0.3 && acorns[a].rotation < 0 ||
+        acorns[a].angle > 0.3 && acorns[a].rotation > 0) {
+      acorns[a].rotation = -acorns[a].rotation;
+    }
+  }
+
+  // Spawn acorn
+  // TODO: Base this off of difficulty?
+  // TODO: Base this off of enemies on-screen?
+  if (player.playTime % 60 === 0) {
+    newAcorn();
+  }
 });
 
 game.render(function (ctx) {
@@ -216,13 +260,22 @@ game.render(function (ctx) {
     helpers.fillRotatedRect(ctx, -0.3 - moveLegsBy, player.x + 9, player.y - 8, 8, 2, player.color);
     helpers.fillRotatedRect(ctx, -0.1 - moveLegsBy, player.x + 11, player.y - 2, 8, 2, player.color);
     helpers.fillRotatedRect(ctx, 0.1 + moveLegsBy, player.x + 11, player.y + 4, 8, 2, player.color);
-    /*ctx.fillRect(player.x - 16, player.y - 5, 6, 1);
-    ctx.fillRect(player.x - 17, player.y - 0, 6, 1);
-    ctx.fillRect(player.x - 16, player.y + 5, 6, 1);
-    ctx.fillRect(player.x + 10, player.y - 5, 6, 1);
-    ctx.fillRect(player.x + 11, player.y - 0, 6, 1);
-    ctx.fillRect(player.x + 10, player.y + 5, 6, 1);
-    */
+  }
+
+  // Draw acorns
+  for (var a = 0; a < acorns.length; a++) {
+    helpers.rotated(ctx, acorns[a].x, acorns[a].y, acorns[a].angle + 0.3, function (x, y) {
+      helpers.scaled(ctx, x, y, 0.85, 1, function (x, y) {
+        ctx.fillStyle = '#FFDA96';  // #fc3
+        helpers.fillCircle(ctx, x, y, acorns[a].r);
+        helpers.fillCircle(ctx, x, y + acorns[a].r - (acorns[a].r / 8), acorns[a].r / 4);
+        ctx.fillStyle = '#492F25';
+        helpers.scaled(ctx, x, y - 14, 1, 0.65, function (x, y) {
+          helpers.fillCircle(ctx, x, y + (acorns[a].r / 2), acorns[a].r, null, Math.PI, 0);
+        });
+        helpers.fillCircle(ctx, x, y - acorns[a].r - (acorns[a].r / 8), acorns[a].r / 4);
+      });
+    });
   }
 
   // Draw particles
